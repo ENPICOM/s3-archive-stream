be6a663b904aff309e920b9c19dd2b03d132f0f9
Bart Versteeg <8367621+bmjhversteeg@users.noreply.github.com>
2025-12-08 10:40:03 +0100

    fix: format
    Signed-off-by: Bart Versteeg <8367621+bmjhversteeg@users.noreply.github.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,7 +11,10 @@ let s3MockClient: S3Client;
 
 describe('s3-archive-stream tests', async () => {
     beforeAll(async () => {
-        s3MockClient = new S3Client({ forcePathStyle: true, endpoint: process.env["S3_MOCK_ENDPOINT"] ?? 'http://localhost:9090' });
+        s3MockClient = new S3Client({
+            forcePathStyle: true,
+            endpoint: process.env['S3_MOCK_ENDPOINT'] ?? 'http://localhost:9090',
+        });
 
         const mockS3Buckets = {
             ['mocked-bucket-1']: [
